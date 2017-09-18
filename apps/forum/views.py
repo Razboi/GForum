f9333c2efa13cb6304a7c240e0f2231d679248ce
Marc <recatala.marc@gmail.com>
2017-09-18 15:06:10 +0200

    Customized style

--- apps/forum/views.py
+++ apps/forum/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+
 from .models import Forum
 from apps.posts.models import Post
+from apps.comments.models import Comment
 
 
 class ForumList(ListView):
@@ -23,4 +25,8 @@ class ForumDetails(ListView):
         slug = self.kwargs.get("slug")
         forum = Forum.objects.get(slug__iexact=slug)  # get the forum with the slug that we are using
         context["title"] = forum.name  # get the current forum name to use it as title
+
+        post = Post.objects.filter(forum__slug__iexact=slug)
+        post_comments = Comment.objects.filter(post=post)
+        context["comment_list"] = post_comments
         return context
