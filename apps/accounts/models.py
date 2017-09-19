857a47a077ce279bfda7f3de704486be39c5e8e6
Marc <recatala.marc@gmail.com>
2017-09-19 14:18:20 +0200

    Added user profile, page logos and customized the style

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    image = models.ImageField(upload_to="profile/", blank=True)
+
+    def __str__(self):
+        return self.user.username
