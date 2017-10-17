4e4193254bf143975e23c97c59eca4108785dcad
Marc <recatala.marc@gmail.com>
2017-10-17 14:39:53 +0200

    Created the option to reset your password by email.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,9 @@
-from django.conf.urls import url
-from .views import login_view, logout_view, register_view, UserProfileView, UpdateProfile, activate
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+
+from .views import (login_view, logout_view, register_view, UserProfileView,
+                    UpdateProfile, activate)
 
 
 urlpatterns = [
@@ -8,6 +12,20 @@ urlpatterns = [
     url(r'^register/$', register_view, name="register"),
     url(r'^profile/(?P<user>[\w-]+)/$', UserProfileView.as_view(), name="profile"),
     url(r'^profile/settings/(?P<user>[\w-]+)/$', UpdateProfile.as_view(), name="settings"),
+
+
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    activate, name="activate"),
+        activate, name="activate"),
+
+
+    url(r'^password_reset/$', auth_views.PasswordResetView.as_view(
+        success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(
+            success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+
+
 ]
