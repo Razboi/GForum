422d8fbb929490691d2838ea1040d376d8c6b363
Marc <recatala.marc@gmail.com>
2017-10-23 21:23:07 +0200

    Changed posts title length

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -10,7 +10,7 @@ User = settings.AUTH_USER_MODEL
 
 
 class Post(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User)
     forum = models.ForeignKey(Forum)
