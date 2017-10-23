d3ec3c8fd7160abde5fe09a2febf29552f424213
Marc <recatala.marc@gmail.com>
2017-10-23 20:21:35 +0200

    Configured AWS for static files

--- config/settings/production.py
+++ config/settings/production.py
@@ -138,25 +138,27 @@ SECURE_HSTS_SECONDS             = 1000000
 SECURE_FRAME_DENY               = True
 
 # aws
-DEFAULT_FILE_STORAGE = “storages.backends.s3boto3.S3Boto3Storage”
-STATICFILES_STORAGE = “storages.backends.s3boto3.S3Boto3Storage”
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = os.environ.get(“AWS_ACCESS_KEY_ID”)
-AWS_SECRET_ACCESS_KEY = os.environ.get(“AWS_SECRET_ACCESS_KEY”)
-AWS_STORAGE_BUCKET_NAME = os.environ.get(“AWS_STORAGE_BUCKET_NAME”)
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 
-AWS_QUERYSTRING_AUTH = False //This will make sure that the file URL does not have unnecessary parameters like your access key.
+AWS_QUERYSTRING_AUTH = False
 
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com’
+AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ".s3.amazonaws.com"
 
 #static media settings
-STATIC_URL = ‘https://' + AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com/’
-MEDIA_URL = STATIC_URL + ‘media/’
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, “static”), )
-STATIC_ROOT = ‘staticfiles’
-ADMIN_MEDIA_PREFIX = STATIC_URL + ‘admin/’
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL + 'static/'
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
+STATIC_ROOT = 'staticfiles'
+ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
 
 STATICFILES_FINDERS = (
-‘django.contrib.staticfiles.finders.FileSystemFinder’,
-‘django.contrib.staticfiles.finders.AppDirectoriesFinder’,
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
